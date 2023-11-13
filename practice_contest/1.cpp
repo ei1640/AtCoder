013054fa180461dd4bdcc17e865a2c42a8420365
Yuri <ei1640.yuri@gmail.com>
2023-11-14 00:41:15 +0900

    add practice_contest dir

---
+++ practice_contest/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+
+    int a, b, c;
+    string str;
+    
+    cin >> a;
+    cin >> b >> c;
+    cin >> str;
+    cout << a + b + c << " " << str << endl;
+
+    return(0);
+}
